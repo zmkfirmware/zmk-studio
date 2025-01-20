0662e6d506715232b1cee872f7354435c0cdea16
Nick Winans <nick.win999@gmail.com>
2025-01-20 14:26:41 -0800

    Support token auth on release fetch (#126)

--- scripts/generate-release-data.js
+++ scripts/generate-release-data.js
@@ -9,13 +9,23 @@ async function generateReleaseData() {
   try {
     const response = await fetch(
       "https://api.github.com/repos/zmkfirmware/zmk-studio/releases/latest",
+      {
+        headers: process.env.GITHUB_TOKEN
+          ? { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` }
+          : {},
+      },
     );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const data = await response.json();
-    const dataFilePath = path.resolve(__dirname, "src", "data", "release-data.json");
+    const dataFilePath = path.resolve(
+      __dirname,
+      "src",
+      "data",
+      "release-data.json",
+    );
     await fs.mkdir(path.dirname(dataFilePath), { recursive: true });
     await fs.writeFile(dataFilePath, JSON.stringify(data));
 
