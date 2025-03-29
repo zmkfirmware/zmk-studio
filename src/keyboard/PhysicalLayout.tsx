c34a20c618004480bb701a48dada6381216189ff
Will Hack <18036720+willhack@users.noreply.github.com>
2025-03-29 11:01:46 +0100

    Revert "fix: keys not rerendering between layers"
    This reverts commit f4f46cbcaf68ab3e008432cd35f634a307bae2a1.

--- src/keyboard/PhysicalLayout.tsx
+++ src/keyboard/PhysicalLayout.tsx
@@ -48,14 +48,14 @@ function scalePosition(
   { x, y, r, rx, ry }: PhysicalLayoutPositionLocation,
   oneU: number,
 ): CSSProperties {
-  const left = x * oneU;
-  const top = y * oneU;
+  let left = x * oneU;
+  let top = y * oneU;
   let transformOrigin = undefined;
   let transform = undefined;
 
   if (r) {
-    const transformX = ((rx || x) - x) * oneU;
-    const transformY = ((ry || y) - y) * oneU;
+    let transformX = ((rx || x) - x) * oneU;
+    let transformY = ((ry || y) - y) * oneU;
     transformOrigin = `${transformX}px ${transformY}px`;
     transform = `rotate(${r}deg)`;
   }
@@ -115,16 +115,16 @@ export const PhysicalLayout = ({
   }, [props.zoom]);
 
   // TODO: Add a bit of padding for rotation when supported
-  const rightMost = positions
+  let rightMost = positions
     .map((k) => k.x + k.width)
     .reduce((a, b) => Math.max(a, b), 0);
-  const bottomMost = positions
+  let bottomMost = positions
     .map((k) => k.y + k.height)
     .reduce((a, b) => Math.max(a, b), 0);
 
   const positionItems = positions.map((p, idx) => (
     <div
-      key={`${p.header}${idx}`}
+      key={idx}
       onClick={() => onPositionClicked?.(idx)}
       className="absolute data-[zoomer=true]:hover:z-[1000] leading-[0]"
       data-zoomer={hoverZoom}
