fdba9e67ee27c0d04a66453b493e80bc7d6b7cd7
Will Hack <18036720+willhack@users.noreply.github.com>
2025-03-31 22:09:43 +0200

    fix: jsx-no-target-blank

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,6 @@ export default defineConfig([
     rules: {
       "no-constant-condition": "off",
       "react/jsx-key": "off",
-      "react/jsx-no-target-blank": "off",
       "react/no-unescaped-entities": "off",
       "react-hooks/exhaustive-deps": "off",
       "react-hooks/rules-of-hooks": "off",
