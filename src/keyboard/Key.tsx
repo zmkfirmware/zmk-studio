4068e1a139c02c763d6191d00a96ea528d4fd6fd
Daniel Svane <danielsvane@gmail.com>
2025-11-19 19:33:25 +0100

    Changed ring color on hover to light gray

--- src/keyboard/Key.tsx
+++ src/keyboard/Key.tsx
@@ -23,7 +23,7 @@ export const Key = ({
 
   return (
     <button
-      className={`group rounded relative flex justify-center items-center cursor-pointer transition-all hover:shadow-xl hover:ring-1 hover:ring-black/5 hover:scale-125 ${selected ? "bg-primary text-primary-content" : "bg-base-100 text-base-content"
+      className={`group rounded relative flex justify-center items-center cursor-pointer transition-all hover:shadow-xl hover:ring-1 hover:ring-gray-300 hover:scale-125 ${selected ? "bg-primary text-primary-content" : "bg-base-100 text-base-content"
         }`}
       style={{
         width: `${pixelWidth}px`,
