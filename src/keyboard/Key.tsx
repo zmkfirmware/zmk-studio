46acf0874ef52024d768fa51c9972ba450e3dd60
Andrew Kannan <andrew.kannan@gmail.com>
2025-11-19 17:57:01 -0500

    feat: Add headers to key in resting / non hover state
    Defines short names for behaviors for display on key headers

--- src/keyboard/Key.tsx
+++ src/keyboard/Key.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from "react";
+import BehaviorShortNames from "./behavior-short-names.json";
 
 interface KeyProps {
   selected?: boolean;
@@ -7,6 +8,30 @@ interface KeyProps {
   oneU: number;
   header?: string;
   onClick?: () => void;
+}
+
+interface BehaviorShortName {
+  short?: string;
+}
+
+const MAX_HEADER_LENGTH = 9;
+const shortNames: Record<string, BehaviorShortName> = BehaviorShortNames;
+
+const shortenHeader = (header: string | undefined) => {
+  if(typeof header === "undefined"){
+    return "";
+  }
+  // Empty string is a valid header for behaviors where we don't want to see a header, which is falsy
+  // So we use an undefined check here
+  if(typeof shortNames[header]?.short !== "undefined"){
+    return shortNames[header].short;
+  } else if(header.length > MAX_HEADER_LENGTH){
+    const words = header.split(/[\s,-]+/);
+    const lettersPerWord = Math.trunc(MAX_HEADER_LENGTH / words.length);
+    return words.map((word) => (word.substring(0,lettersPerWord))).join("");
+  } else {
+    return header;
+  }
 }
 
 export const Key = ({
@@ -31,7 +56,7 @@ export const Key = ({
       }}
       onClick={onClick}
     >
-      <div className={`absolute text-xs ${selected ? "text-primary-content" : "z1text-base-content"} opacity-0 group-hover:opacity-80 top-1 text-nowrap left-1/2 font-light -translate-x-1/2 text-center transition-opacity`}>{header}</div>
+      <div className={`absolute text-xs ${selected ? "text-primary-content" : "z1text-base-content"} opacity-80 top-1 text-nowrap left-1/2 font-light -translate-x-1/2 text-center`}>{shortenHeader(header)}</div>
       {children}
     </button>
   );
