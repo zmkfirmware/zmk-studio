8bc19a51e54d9e06fe36640e8496880ba78eb30b
Andrew Kannan <andrew.kannan@gmail.com>
2025-11-25 17:36:11 -0500

    feat: update HID usage input to use grid of buttons

--- src/hid-usages.ts
+++ src/hid-usages.ts
@@ -1,15 +1,16 @@
 // import { UsagePages } from "./HidUsageTables-1.5.json";
 // Filtered with `cat src/HidUsageTables-1.5.json | jq '{ UsagePages: [.UsagePages[] | select([.Id] |inside([7, 12]))] }' > src/keyboard-and-consumer-usage-tables.json`
 import { UsagePages } from "./keyboard-and-consumer-usage-tables.json";
-import HidOverrides from "./hid-usage-name-overrides.json";
+import HidSupplementaryMetadata from "./hid-usage-metadata.json";
 
-interface HidLabels {
+interface HidMetadata {
   short?: string;
   med?: string;
   long?: string;
+  category?: string;
 }
 
-const overrides: Record<string, Record<string, HidLabels>> = HidOverrides;
+const overrides: Record<string, Record<string, HidMetadata>> = HidSupplementaryMetadata;
 
 export interface UsageId {
   Id: number;
@@ -41,12 +42,21 @@ export const hid_usage_get_label = (
     (u) => u.Id === usage_id
   )?.Name;
 
-export const hid_usage_get_labels = (
+export const hid_usage_get_metadata = (
   usage_page: number,
   usage_id: number
-): { short?: string; med?: string; long?: string } =>
-  overrides[usage_page.toString()]?.[usage_id.toString()] || {
-    short: UsagePages.find((p) => p.Id === usage_page)?.UsageIds?.find(
-      (u) => u.Id === usage_id
-    )?.Name,
-  };
+): { short?: string; med?: string; long?: string, category?: string } => {
+  if(overrides[usage_page.toString()]?.[usage_id.toString()]?.short) {
+    return overrides[usage_page.toString()]?.[usage_id.toString()];
+  } else {
+    const fullName = UsagePages.find((p) => p.Id === usage_page)?.UsageIds?.find(
+        (u) => u.Id === usage_id
+      )?.Name
+    return {
+      short: fullName?.replace(/^Keyboard /, ""),
+      med: fullName?.replace(/^Keyboard /, ""),
+      long: fullName,
+      category: overrides[usage_page.toString()]?.[usage_id.toString()]?.category || "Other"
+    }
+  }
+}
